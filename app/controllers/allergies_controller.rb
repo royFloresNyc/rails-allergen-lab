b2b8d67a0b18b15889bcba1941ca3e895d3ff375
royFloresNyc <royfloresnyc@gmail.com>
2020-09-01 17:32:52 -0400

    Add buttons

--- app/controllers/allergies_controller.rb
+++ app/controllers/allergies_controller.rb
@@ -1,2 +1,13 @@
 class AllergiesController < ApplicationController
+
+    def new
+        @allergy = Allergy.new
+        @users = User.all
+        @ingredients = Ingredient.all
+    end 
+
+    def create
+        allergy = Allergy.create(params.require(:allergy).permit(:user_id, :ingredient_id))
+        redirect_to user_path(allergy.user)
+    end 
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,19 @@ class RecipesController < ApplicationController
     def show
         @recipe = Recipe.find(params[:id])
     end 
+
+    def new
+        @recipe = Recipe.new
+    end 
+
+    def create
+        @recipe = Recipe.create(recipe_params)
+
+    end 
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:user_id, :title )
+    end 
 end
