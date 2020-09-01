6a560b88842d7fe6fcc79863b55c63b5b7fc65f8
royFloresNyc <royfloresnyc@gmail.com>
2020-08-31 22:41:40 -0400

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Recipe.destroy_all
+Ingredient.destroy_all
+Allery.destroy_all
+
+10.times do 
+    User.create(name: Faker::Name.unique.name, age: rand(6..65))
+end 
+
+5.times do 
+    Recipe.create(user_id: User.all.sample, title: Faker::Food.unique.dish)
+end 
+
+10.times do 
+    Ingredient.create(name: Faker::Food.unique.ingredient, recipe: Recipe.all.sample)
+end 
+
+10.times do 
+    Allergy.create(user: User.all.sample, ingredient: Ingredient.all.sample)
+end 
