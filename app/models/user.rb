b2fa856381450ab6653eca9ba28e23de3ddf7d34
royFloresNyc <royfloresnyc@gmail.com>
2020-09-01 00:21:44 -0400

    User views and functionalities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    has_many :recipes
-    has_many :allergies
+    has_many :recipes, foreign_key: :user_id, dependent: :destroy
+    has_many :allergies, foreign_key: :user_id, dependent: :destroy
 
     def allergic_to
-        self.allergies.map { |allergy| allergy.ingredient.name }
+        self.allergies.map { |allergy| allergy.ingredient }
     end 
 end
